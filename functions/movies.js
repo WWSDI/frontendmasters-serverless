3f4642846bf5162eabb1d402de68fc6906ac16e6
Shawn DI <ShawnsuDI@Shawns-MBP13-2018.local>
2022-07-03 16:18:06 +1200

    refactor: replace movie.json with cloud db in Heroku&Hasura

--- functions/movies.js
+++ functions/movies.js
@@ -1,27 +1,40 @@
 const { URL } = require("url");
 const fetch = require("node-fetch");
-// still using the movies from our own database
-const movies = require("../data/movies");
-
-const api = new URL("https://www.omdbapi.com/");
-// add secret API key to the query string
-api.searchParams.set("apikey", process.env.OMDB_API_KEY);
-
-// we are fetching user rating info from OMDB, and insert it into each movie data
-const promisesOfMovies = movies.map((movie) => {
-  api.searchParams.set("i", movie.id);
-
-  return fetch(api)
-    .then((res) => res.json())
-    .then((data) => {
-      return {
-        ...movie,
-        scores: data.Ratings,
-      };
-    });
-});
+// using a query() to get own movie data from Hasura
+const { query } = require("./util/hasura");
 
 exports.handler = async () => {
+  const { movies } = await query({
+    query: `
+      query AllMovies {
+        movies {
+          id
+          poster
+          tagline
+          title
+        }
+    }
+    `,
+  });
+
+  const api = new URL("https://www.omdbapi.com/");
+  // add secret API key to the query string
+  api.searchParams.set("apikey", process.env.OMDB_API_KEY);
+
+  // we are fetching user rating info from OMDB, and insert it into each movie data
+  const promisesOfMovies = movies.map((movie) => {
+    api.searchParams.set("i", movie.id);
+
+    return fetch(api)
+      .then((res) => res.json())
+      .then((data) => {
+        return {
+          ...movie,
+          scores: data.Ratings,
+        };
+      });
+  });
+
   const moviesWithRatings = await Promise.all(promisesOfMovies);
   return {
     statusCode: 200,
