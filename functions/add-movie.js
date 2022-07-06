beb605d6feb47d9a102c9114c6cb513961dc392d
Shawn DI <ShawnsuDI@Shawns-MBP13-2018.local>
2022-07-07 09:14:44 +1200

    feat: add a movie to db via hasura

---
+++ functions/add-movie.js
@@ -0,0 +1,25 @@
+const { query } = require("./util/hasura");
+
+exports.handler = async (event) => {
+  const { id, title, tagline, poster } = event.body;
+  // console.log("🌸", event.body);
+
+  const result = await query({
+    query: `
+    mutation MyMutation($id: String!, $poster: String!, $tagline: String!, $title: String!) {
+  insert_movies_one(object: {id: $id, poster: $poster, tagline: $tagline, title: $title}) {
+    id
+    poster
+    tagline
+    title
+  }
+}`,
+    variables: JSON.parse(event.body),
+  });
+  // console.log("🌸", result);
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(result),
+  };
+};
