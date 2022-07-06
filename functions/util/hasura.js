1cf415a2e69152a835d1b6c59e01851091c2d886
Shawn DI <ShawnsuDI@Shawns-MBP13-2018.local>
2022-07-07 09:14:02 +1200

    fix: incorrect variable name

--- functions/util/hasura.js
+++ functions/util/hasura.js
@@ -1,13 +1,13 @@
 const fetch = require("node-fetch");
 
-async function query({ query, variabes = {} }) {
+async function query({ query, variables = {} }) {
   const result = await fetch(process.env.HASURA_API_URL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
       "X-Hasura-Admin-Secret": process.env.HASURA_ADMIN_SECRET,
     },
-    body: JSON.stringify({ query, variabes }),
+    body: JSON.stringify({ query, variables }),
   }).then((res) => res.json());
 
   // TODO: show helpful info if there's an error
