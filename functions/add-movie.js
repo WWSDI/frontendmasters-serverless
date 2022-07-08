c294fca4f24ed368166cabc1efe13ac0cd70d0bd
Shawn DI <ShawnsuDI@Shawns-MBP13-2018.local>
2022-07-08 15:48:21 +1200

    feat: add-movie query with role-based authentication

--- functions/add-movie.js
+++ functions/add-movie.js
@@ -1,18 +1,24 @@
-const { query } = require("./util/hasura");
+const { query: gqlQuery } = require("./util/hasura");
 
-exports.handler = async (event) => {
-  // console.log("🌸", event.body);
-
-  const result = await query({
+const addMovieQuery = async (event) => {
+  const result = await gqlQuery({
     query: `
-    mutation MyMutation($id: String!, $poster: String!, $tagline: String!, $title: String!) {
-  insert_movies_one(object: {id: $id, poster: $poster, tagline: $tagline, title: $title}) {
-    id
-    poster
-    tagline
-    title
-  }
-}`,
+        mutation MyMutation(
+          $id: String!
+          $poster: String!
+          $tagline: String!
+          $title: String!
+        ) {
+          insert_movies_one(
+            object: { id: $id, poster: $poster, tagline: $tagline, title: $title }
+          ) {
+            id
+            poster
+            tagline
+            title
+          }
+        }
+      `,
     variables: JSON.parse(event.body),
   });
   // console.log("🌸", result);
@@ -21,4 +27,21 @@ exports.handler = async (event) => {
     statusCode: 200,
     body: JSON.stringify(result),
   };
+};
+const test401Error = (isLoggedIn, roles) => {
+  if (!isLoggedIn || !roles.includes("admin"))
+    return {
+      statusCode: 401,
+      body: "Unauthorized",
+    };
+  return false;
+};
+
+exports.handler = async (event, context) => {
+  const { user } = context.clientContext;
+  const isLoggedIn = user && user.app_metadata;
+  const roles = user.app_metadata.roles || [];
+
+  let result = test401Error(isLoggedIn, roles) || addMovieQuery(event);
+  return result;
 };
